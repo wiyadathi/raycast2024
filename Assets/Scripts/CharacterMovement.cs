71ba7097f9da7ce6cac90b0daef52f1889b66e61
wiyadathi <wiyada.t@bu.ac.th>
2024-01-20 18:12:07 +0700

    Character Movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 3f;
+    public float rotationSpeed = 500f;
+
+    private CharacterController characterController;
+
     void Start()
     {
-        
+        // Get the CharacterController component attached to the GameObject
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get input from the player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction based on input
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+       /* // If there's any input, rotate the character to face the movement direction
+        if (movement.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+*/
+        // Move the character in the calculated direction
+        characterController.Move(movement * speed * Time.deltaTime);
     }
 }

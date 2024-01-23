ce35a0907260b5475fd44c58065ac341d8334a5d
wiyadathi <wiyada.t@bu.ac.th>
2024-01-23 13:29:10 +0700

    Edit Raycast Code

--- Assets/Scripts/CubeRaycast.cs
+++ Assets/Scripts/CubeRaycast.cs
@@ -8,14 +8,14 @@ public class CubeRaycast : MonoBehaviour
     void Update()
     {
         //GetComponent<Rigidbody>().AddTorque(15f, 0, 15f);
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward * 20), Color.green);
+        Debug.DrawRay(transform.position, transform.forward * 20, Color.green);
 
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo, 30f))
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, 30f))
         {
             Debug.Log($"Hit2 {hitInfo.collider.gameObject.tag}");
             if (hitInfo.collider.tag == "Star")
             {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward * 20), Color.red);
+                Debug.DrawRay(transform.position, transform.forward * 20, Color.red);
                 hitInfo.collider.gameObject.GetComponent<Renderer>().material.color = Color.red;
                 hitInfo.collider.gameObject.GetComponent<Rigidbody>().useGravity = true;
             }
--- Assets/Scripts/ShootRaycast.cs
+++ Assets/Scripts/ShootRaycast.cs
@@ -9,11 +9,15 @@ public class ShootRaycast : MonoBehaviour
 
     void Update()
     {
+        Debug.DrawRay(shootPoint.transform.position, 
+            transform.forward * 20f, Color.green);
+        
         if (Physics.Raycast(shootPoint.transform.position, 
-            transform.TransformDirection(Vector3.forward),
-            out RaycastHit hitInfo, 20f, _layerMask))
+            transform.forward, out RaycastHit hitInfo, 
+            20f, _layerMask))
         {
             Debug.Log($"Hit {hitInfo.collider.gameObject.tag}!");
+            //alternative code for transform.forward
             Debug.DrawRay(shootPoint.transform.position, 
                           transform.TransformDirection(Vector3.forward) * hitInfo.distance, 
                           Color.red );
@@ -21,9 +25,8 @@ public class ShootRaycast : MonoBehaviour
         else
         {
             Debug.Log("hit nothing.");
-            Debug.DrawRay(shootPoint.transform.position, 
-                          transform.TransformDirection(Vector3.forward) * 20f, 
-                          Color.green);
         }
     }
 }
+
+
